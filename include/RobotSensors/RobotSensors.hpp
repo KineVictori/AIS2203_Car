f067407348232a11919a0992794dc51681475da6
Kine <kine.aurora@hotmail.com>
2025-11-17 19:11:38 +0100

    filoppsett

---
+++ include/RobotSensors/RobotSensors.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Kinea on 17.11.2025.
+//
+
+#ifndef ROBOTSENSORS_HPP
+#define ROBOTSENSORS_HPP
+
+
+
+class RobotSensors {
+
+};
+
+
+
+#endif //ROBOTSENSORS_HPP
--- src/Lidar/LidarData.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Kinea on 17.11.2025.
-//
-
-#include "../../include/Lidar/LidarData.hpp"
