d22d6c774adeea8ebcccc5c0feaa8df93f177252
Kine <kine.aurora@hotmail.com>
2025-11-22 13:33:04 +0100

    start MasterMind.cpp

---
+++ executables/MasterMind/MasterMind.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <algorithm>
+#include <ros/ros.h>
+#include "RobotSensors/RobotSensors.hpp"
+#include "GeneralComunication/GeneralComunication.hpp"
+#include "Vision/Vision.hpp"
+
+int main(int argc, char **argv) {
+
+    Vision vision;
+    GeneralComunication genCom{30123};
+    Data data;
+
+    ros::init(argc, argv, "testRobotSensors");
+    ros::NodeHandle n;
+    RobotSensors sensors(n);
+
+    ros::Rate loop_rate(10);
+    while (true) {
+        if (!vision.isFinished()) {
+            vision.update();
+
+        }
+
+        if (ros::ok()) {
+            ros::spinOnce();
+
+            // Access latest data
+            sensor_msgs::Imu imu = sensors.getLatestImu();
+            sensor_msgs::LaserScan scan = sensors.getLatestScan();
+            nav_msgs::Odometry odom = sensors.getLatestOdom();
+
+            // bare tester, endre senere til faktisk bruk
+            ROS_INFO_STREAM("IMU Acceleration X: " << imu.linear_acceleration.x);
+            ROS_INFO_STREAM("LaserScan Min Range: " << (scan.ranges.empty() ? 0 : *std::min_element(scan.ranges.begin(), scan.ranges.end())));
+            ROS_INFO_STREAM("Odometry X: " << odom.pose.pose.position.x);
+
+        }
+
+        data.fromJson();
+
+        loop_rate.sleep();
+    }
+    return 0;
+}
