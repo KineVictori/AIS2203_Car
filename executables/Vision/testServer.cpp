8950abfb90ee296a1b2a175bb900c13b5ad1d8d0
Øystein Bringsli <oysteinmb@gmail.com>
2025-11-24 14:40:28 +0100

    tmp

--- executables/Vision/testServer.cpp
+++ executables/Vision/testServer.cpp
@@ -33,10 +33,10 @@ int main() {
 
 
         // If connected to a display, you may uncomment these lines.
-        /*auto frame = vision.getFrame();
+        // auto frame = vision.getFrame();
         if(frame.empty()) break;
         cv::imshow("CSI Camera", frame);
-        if(cv::waitKey(1) == 27) break; // ESC to exit*/
+        if(cv::waitKey(1) == 27) break; // ESC to exit
     }
 
     return 0;
