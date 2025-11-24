e5d476cbc93a5a1fc58df453a7cf5bed6cfe5294
Øystein Bringsli <oysteinmb@gmail.com>
2025-11-24 13:28:56 +0100

    tmp

--- executables/Vision/testServer.cpp
+++ executables/Vision/testServer.cpp
@@ -21,6 +21,26 @@ int main() {
             std::vector<Detection> output = inference.runInference(frame);
             int detections = output.size();
             std::cout << "Number of detections:" << detections << std::endl;
+
+            for (int i = 0; i < detections; ++i)
+            {
+                Detection detection = output[i];
+
+                cv::Rect box = detection.box;
+                cv::Scalar color = detection.color;
+
+                // Detection box
+                cv::rectangle(frame, box, color, 2);
+
+                // Detection box text
+                std::string classString = detection.className + ' ' + std::to_string(detection.confidence).substr(0, 4);
+                cv::Size textSize = cv::getTextSize(classString, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
+                cv::Rect textBox(box.x, box.y - 40, textSize.width + 10, textSize.height + 20);
+
+                std::cout << classString << std::endl;
+                //cv::rectangle(frame, textBox, color, cv::FILLED);
+                //cv::putText(frame, classString, cv::Point(box.x + 5, box.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
+            }
         } catch (...) {}
 
 
