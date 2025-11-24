7d39fbe3d6e748a7dbeef76957505355462de9f3
Øystein Bringsli <oysteinmb@gmail.com>
2025-11-24 13:46:29 +0100

    tmp

--- src/ModelEstimation/inference.cpp
+++ src/ModelEstimation/inference.cpp
@@ -32,7 +32,7 @@ std::vector<Detection> Inference::runInference(const cv::Mat &input)
     int rows = outputs[0].size[1];
     int dimensions = outputs[0].size[2];
 
-    bool yolov8 = false;
+    bool yolov8 = true;
     // yolov5 has an output of shape (batchSize, 25200, 85) (Num classes + box[x,y,w,h] + confidence[c])
     // yolov8 has an output of shape (batchSize, 84,  8400) (Num classes + box[x,y,w,h])
     if (dimensions > rows) // Check if the shape[2] is more than shape[1] (yolov8)
